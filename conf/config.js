077a0224d88df9f7e00261e4bea2b87ec0321cb6
koujp <koujp@qq.com>
2017-05-10 18:07:09 +0800

    config example update

--- conf/config.js
+++ conf/config.js
@@ -43,10 +43,10 @@ const config = {
       port:[8080], //服务器监听端口,可配置多个
       attributes:{anonymous:false}, //配置自定义属性
 	  proxy:{
-		pathRule:null, //配置需要代理url的匹配规则，为正则表达式
-        server:'172.24.63.42', //配置服务端IP
+		pathRule:null,//【required】配置需要代理url的匹配规则，为正则表达式
+        server:'192.168.1.100', //配置服务端IP
         port:80, //【optional】配置服务端端口，如不配置则与请求端口一致
-        headers:[] //【optional】配置发送请求时需要添加的header
+        headers:{} //【optional】配置发送请求时需要添加的header
 	  }
     }
   ]
