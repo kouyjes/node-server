75620c16f996899e4f5abe479ec22e518bf5bb70
koujp <koujp@qq.com>
2017-06-06 17:42:02 +0800

    server log config

--- slib/server-logger.js
+++ slib/server-logger.js
@@ -4,6 +4,9 @@ const log4js = require('log4js');
 log4js.loadAppender('file');
 log4js.configure(serverConfig.logFilePath, { reloadSecs: 300 });
 var logger = log4js.getLogger('server-sys');
-exports.getLogger = function () {
+exports.getLogger = function (category) {
+    if(typeof category === 'string'){
+        return log4js.getLogger(category);
+    }
     return logger;
 };
